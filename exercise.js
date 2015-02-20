d91de6c9e5fb8d804337e5a3b2cfee7770837941
Christophe Porteneuve <tdd@tddsworld.com>
2015-02-20 08:52:28 +0100

    Moving from `.basic()` to `require("workshopper-exercise/basic")` as per #18.

--- exercise.js
+++ exercise.js
@@ -266,18 +266,5 @@ Exercise.prototype.end = function (mode, pass, callback) {
   })(0)
 }
 
-Exercise.basic = function basicExercise () {
-  var exercise      = new Exercise()
-    , filecheck     = require('./filecheck')
-    , execute       = require('./execute')
-    , comparestdout = require('./comparestdout')
-
-  exercise = filecheck(exercise)
-  exercise = execute(exercise)
-  exercise = comparestdout(exercise)
-
-  return exercise
-}
-
 
 module.exports = Exercise
